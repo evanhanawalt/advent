e28fdf3e42b7fc30a7ef1ade078b02cc90f339ee
evanhanawalt <evanthanawalt@gmail.com>
2022-12-14 17:23:33 -0500

    rust 1 and 2

--- rust/1/1.rs
+++ rust/1/1.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Helasdasdasdlo, world!");
+
+use std::fs;
+fn get_inv() -> Vec<Vec<u32>> {
+    let contents = fs::read_to_string("./input.txt").unwrap();
+    let mut inventories: Vec<Vec<u32>> = Vec::new();
+    let mut current = 0;
+    for line in contents.trim().lines() {
+        if current == inventories.len() {
+            inventories.push(Vec::new());
+        }
+        if line.is_empty() { current+=1 }
+        if let Ok(n) = line.parse() {
+            inventories[current].push(n);
+        }
+    }
+    
+    return inventories;
+}
+fn part1() {
+    // Read input and create the inventories
+    let inventories = get_inv();
+    let res: u32 = inventories.iter().map(|v| v.iter().sum()).max().unwrap();
+    println!("Part1: {}", res);
+}
+
+fn part2() {
+    let inventories = get_inv();
+    let mut sums:Vec<u32> = inventories.iter().map(|v| v.iter().sum()).collect();
+    sums.sort();
+    sums.reverse();
+    let res: u32 = sums.iter().take(3).sum();
+    println!("Part2: {}" ,res);
+}
+
+
+fn main (){
+    part1();
+    part2();
 }
---
+++ rust/2/2.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+fn read_input () -> Vec<String> {
+    let contents = std::fs::read_to_string("./input.txt").unwrap();
+    let ret: Vec<String> = contents.lines().map(str::to_string).collect();
+    return ret;
+    
+}
+fn part1() {
+    let lines = read_input();
+    let mut score_map:HashMap<String, i32> = HashMap::new();
+    score_map.insert(String::from("A X"), 4);
+    score_map.insert(String::from("A Y"), 8);
+    score_map.insert(String::from("A Z"), 3);
+    score_map.insert(String::from("B X"), 1);
+    score_map.insert(String::from("B Y"), 5);
+    score_map.insert(String::from("B Z"), 9);
+    score_map.insert(String::from("C X"), 7);
+    score_map.insert(String::from("C Y"), 2);
+    score_map.insert(String::from("C Z"), 6);
+    
+    let mut score:i32 = 0;
+    for line in lines.iter() {
+        score += score_map.get(line).unwrap();
+    }
+    println!("Part 1: {}", score);
+}
+
+fn part2() {
+    let lines = read_input();
+    let mut score_map:HashMap<String, i32> = HashMap::new();
+    score_map.insert(String::from("A X"), 3);
+    score_map.insert(String::from("A Y"), 4);
+    score_map.insert(String::from("A Z"), 8);
+    score_map.insert(String::from("B X"), 1);
+    score_map.insert(String::from("B Y"), 5);
+    score_map.insert(String::from("B Z"), 9);
+    score_map.insert(String::from("C X"), 2);
+    score_map.insert(String::from("C Y"), 6);
+    score_map.insert(String::from("C Z"), 7);
+    
+    let mut score:i32 = 0;
+    for line in lines.iter() {
+        score += score_map.get(line).unwrap();
+    }
+    println!("Part 2: {}", score);
+}
+fn main() {
+    part1();
+    part2();
+}
