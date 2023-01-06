9eea6cd36a769a74fccbde6a1ed156070274c03a
evanhanawalt <evanthanawalt@gmail.com>
2023-01-05 23:23:23 -0500

    fast 16

--- rust/16/16.rs
+++ rust/16/16.rs
@@ -4,18 +4,16 @@ use regex::Regex;
 
 #[derive(Debug, Clone)]
 struct Valve {
-    index: usize,
-    name: String,
-    flow: u32,
+    flow: i32,
     neighbors: Vec<usize>,
 }
 
 #[derive(Debug, Clone)]
 struct State {
     index: usize,
-    name: String,
-    flow: u32,
-    neighbors: Vec<usize>,
+    flow: i32,
+    time: i32,
+    visited: Vec<usize>,
 }
 
 fn index_mapper(s: &String) -> usize {
@@ -35,25 +33,17 @@ fn get_valves() -> HashMap<usize, Valve> {
         let flow_str = split.nth(2).unwrap();
         let neighbors: Vec<usize> = split.skip(4).map(|s| index_mapper(&s)).collect();
         let index = index_mapper(&name);
-        let flow = flow_str.parse::<u32>().unwrap();
-        valves.insert(
-            index,
-            Valve {
-                index,
-                flow,
-                name,
-                neighbors,
-            },
-        );
+        let flow = flow_str.parse::<i32>().unwrap();
+        valves.insert(index, Valve { flow, neighbors });
     }
     return valves;
 }
 
-fn compute_shortest(valves: &HashMap<usize, Valve>, index: usize) -> HashMap<usize, u32> {
-    let mut shortest_distances: HashMap<usize, u32> = HashMap::new();
+fn compute_shortest(valves: &HashMap<usize, Valve>, index: usize) -> HashMap<usize, i32> {
+    let mut shortest_distances: HashMap<usize, i32> = HashMap::new();
     let mut visited: Vec<usize> = Vec::new();
-    for (k, v) in valves {
-        shortest_distances.insert(*k, u32::MAX);
+    for (k, _v) in valves {
+        shortest_distances.insert(*k, i32::MAX);
     }
     shortest_distances.insert(index, 0);
 
@@ -82,14 +72,12 @@ fn compute_shortest(valves: &HashMap<usize, Valve>, index: usize) -> HashMap<usi
             }
         }
     }
-
     return shortest_distances;
 }
 
-fn get_distances(valves: &HashMap<usize, Valve>) -> HashMap<usize, HashMap<usize, u32>> {
-    // let mut distances: HashMap<usize,<HashMap<usize,u32>>> = HashMap::new();
-    let mut distances: HashMap<usize, HashMap<usize, u32>> = HashMap::new();
-    for (index, v) in valves {
+fn get_distances(valves: &HashMap<usize, Valve>) -> HashMap<usize, HashMap<usize, i32>> {
+    let mut distances: HashMap<usize, HashMap<usize, i32>> = HashMap::new();
+    for (index, _v) in valves {
         distances.insert(*index, compute_shortest(valves, *index));
     }
     return distances;
@@ -97,22 +85,95 @@ fn get_distances(valves: &HashMap<usize, Valve>) -> HashMap<usize, HashMap<usize
 
 fn get_all_paths(
     valves: &HashMap<usize, Valve>,
-    distances: &HashMap<usize, HashMap<usize, u32>>,
-    minutes: u32,
+    distances: &HashMap<usize, HashMap<usize, i32>>,
+    minutes: i32,
 ) -> Vec<State> {
     let mut paths: Vec<State> = Vec::new();
+    let mut unfinished_paths: Vec<State> = Vec::new();
+    unfinished_paths.push(State {
+        index: index_mapper(&String::from("AA")),
+        flow: 0,
+        time: minutes,
+        visited: Vec::new(),
+    });
 
+    let things_to_visit: Vec<usize> = valves
+        .into_iter()
+        .filter(|(_k, v)| v.flow > 0)
+        .map(|(k, _v)| *k)
+        .collect();
+
+    while !unfinished_paths.is_empty() {
+        let current = unfinished_paths.remove(0);
+        // println!("{:?}", current);
+        let options: Vec<usize> = things_to_visit
+            .iter()
+            .filter(|n| !current.visited.contains(n))
+            .cloned()
+            .collect();
+        if options.is_empty() {
+            paths.push(current.clone());
+        } else {
+            for n in &options {
+                let steps = distances.get(&current.index).unwrap().get(n).unwrap() + 1;
+                let new_time = current.time - steps;
+                if new_time <= 0 {
+                    paths.push(current.clone());
+                } else {
+                    let new_flow = current.flow + (valves.get(n).unwrap().flow * new_time);
+                    let mut new_visited = Vec::clone(&current.visited);
+
+                    new_visited.push(*n);
+
+                    // dont bother if there is already a path with these visited with a bigger flow
+                    let add_this: bool = !unfinished_paths.iter().any(|p| {
+                        p.flow > new_flow && p.visited.iter().all(|node| new_visited.contains(node))
+                    });
+
+                    if add_this {
+                        let new_state = State {
+                            flow: new_flow,
+                            time: new_time,
+                            visited: new_visited,
+                            index: *n,
+                        };
+                        unfinished_paths.push(new_state);
+                    }
+                }
+            }
+        }
+    }
     return paths;
 }
 
 fn part1() {
     let valves = get_valves();
     let distances = get_distances(&valves);
-
-    println!("Part1: ");
+    let mut paths = get_all_paths(&valves, &distances, 30);
+    paths.sort_by(|a, b| b.flow.cmp(&a.flow));
+    println!("Part1:{}", paths.get(0).unwrap().flow);
 }
 fn part2() {
-    println!("Part2: ");
+    let valves = get_valves();
+    let distances = get_distances(&valves);
+    let mut paths = get_all_paths(&valves, &distances, 26);
+    paths.sort_by(|a, b| b.flow.cmp(&a.flow));
+
+    'main: for i in 0..paths.len() {
+        for e in (i + 1)..paths.len() {
+            let valid = paths
+                .get(i)
+                .unwrap()
+                .visited
+                .iter()
+                .all(|node| !paths.get(e).unwrap().visited.contains(node));
+            if valid {
+                let result = paths.get(i).unwrap().flow + paths.get(e).unwrap().flow;
+                println!("Part2: {result}");
+                break 'main;
+            }
+        }
+    }
 }
 fn main() {
     part1();
